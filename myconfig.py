292452429499366ce8ab134cf34951fd5800304e
Brandon Tong <brandonidas@gmail.com>
2020-12-13 10:59:54 +0800

    Delete myconfig.py

--- myconfig.py
+++
@@ -1,5 +0,0 @@
-AUTH = "BQCuy-RtD7vKv8wT1UXSYkRdOzKcb7_9Y0Uf2pyj2rz01Uxiunie1kPhk3nGFwFMGg6QmAAev3XVgYoRAPj5UzBugfDCBCVDv3wTzfqR9ZSV91LqF_R278FGsSmvrQAE7b-KeUz6kvsdenew8otwrw"
-USERID = "justdostuff"
-DATABASE_LOCATION = "sqlite:///tracks.sqlite"
-
-# TODO move to gitignore
